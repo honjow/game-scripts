d76fd4f9dbb2aa2808d07e5f6c35576eefc8e7f1
honjow <honjow311@gmail.com>
2024-05-27 04:05:04 +0800

    update

--- win3_hhd.sh
+++ win3_hhd.sh
@@ -14,12 +14,16 @@ l4_r4_code=$(printf '0x%X\n' $(( $(echo $l4_code) + $(echo $r4_code) )))
 hhd_path=/usr/lib/python*/site-packages/hhd
 gpd_base_path="$hhd_path/device/gpd/win/base.py"
 
-ori_l4_code="0x46"
-ori_r4_code="0x48"
-ori_l4_r4_code="0x8E"
+# 分别获取包含 "case 0x" 的行号
+lines=$(grep -n "case 0x" $gpd_base_path | cut -d: -f1)
+# 取前三个行号
+line1=$(echo $lines | cut -d' ' -f1)
+line2=$(echo $lines | cut -d' ' -f2)
+line3=$(echo $lines | cut -d' ' -f3)
 
-sed -i "s/case ${ori_l4_code}/case ${l4_code}/g" $gpd_base_path
-sed -i "s/case ${ori_r4_code}/case ${r4_code}/g" $gpd_base_path
-sed -i "s/case ${ori_l4_r4_code}/case ${l4_r4_code}/g" $gpd_base_path
+# 分别在对应行进行替换, "case 0xXXXX:" -> "case l4_code:"
+sudo sed -i "${line1}s/0x[0-9A-Fa-f]*/$l4_code/" $gpd_base_path
+sudo sed -i "${line2}s/0x[0-9A-Fa-f]*/$r4_code/" $gpd_base_path
+sudo sed -i "${line3}s/0x[0-9A-Fa-f]*/$l4_r4_code/" $gpd_base_path
 
 sudo systemctl restart hhd@$USER.service
